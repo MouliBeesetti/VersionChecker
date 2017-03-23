38d78da58f0f0b2b35336a07da9614fac2418289
Mouli Beesetti <prince.mouli4@gmail.com>
2017-03-23 12:16:10 +0530

    Add files via upload

---
+++ VersionChecker.py
@@ -0,0 +1,12 @@
+import platform
+
+# Python Version
+PyVersion = platform.python_version()
+
+# OS Version
+os = platform.platform()
+
+# Print Statement
+print("---------------------------------------------")
+print(f"Python version : {PyVersion!r}|\nOS = {os!r}")
+print("---------------------------------------------")
